9901c6db94f5ec316d9378cc7094feab7d5b863b
yazdi <aminashofteh@gmail.com>
2020-12-07 20:59:48 +0330

    error handling ok with simple way

--- tn00002/app/Http/Controllers/UserController.php
+++ tn00002/app/Http/Controllers/UserController.php
@@ -1,21 +1,20 @@
 <?php
-	
 	namespace App\Http\Controllers;
-	
 	use Illuminate\Http\Request;
 	use App\User;
 	class UserController extends Controller
 	{
 		public function index(Request $request){
-			
 			$articles = User::all()->toArray();
 			return $articles;
 		}
-		
 		public function show(Request $request, $id) {
-			
-			$user = User::findOrFail($id);
-			return $user;
+			try{
+				$user = User::findOrFail($id);
+				return $user;
+			}
+			catch (\Exception $e){
+				return response(['message'=>'Not Found Resource'], 404);
+			}
 		}
-		
-	}
+	}			
